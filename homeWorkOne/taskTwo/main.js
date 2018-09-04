1a70caf4033eac49708e7ed27dc6150dd3b87b12
Monohansett <Frostyvi92@gmail.com>
2018-09-04 18:08:57 +0300

    hw1 task2 beautify some code

--- homeWorkOne/taskTwo/main.js
+++ homeWorkOne/taskTwo/main.js
@@ -3,27 +3,38 @@ window.onload = function () {
     let changeStyleButton = document.querySelector('.changeStyle__button')
     let renderFlexButton = document.querySelector('.renderFlex__button')
 
+    // assign functions to buttons 
+
     redirectButton.onclick = redirectTo;
     changeStyleButton.onclick = changePageStyle;
     renderFlexButton.onclick = renderFlexboxExample;
 
+    // redirect to another page function
+
     function redirectTo() {
         location.href = "https://www.github.com/Monohansett"
     }
+
+    // change style on page
 
     function changePageStyle() {
         let buttons = document.querySelectorAll('button');
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].classList.toggle('styledButtons')
         }
-        document.querySelector('body').style.backgroundColor = '#c1d4f2';
+        document.body.style.backgroundColor = '#c1d4f2';
     }
 
+    // render new body with flexbox example
+
     function renderFlexboxExample() {
-        document.querySelector('body').innerHTML = "";
+        document.body.innerHTML = "";
+        document.body.style.backgroundColor = "";
         let flexContainer = document.createElement('div');
         let innerFlexBoxes = ['flex1', 'flex2', 'flex3'];
         document.body.appendChild(flexContainer);
+
+        //loop for create multiple items with styling
 
         for (let i=0; i<innerFlexBoxes.length; i++) {
             let flexElement = document.createElement('div');
@@ -34,11 +45,5 @@ window.onload = function () {
 
         flexContainer.style.cssText =  "margin-top: 300px; display: flex; justify-content: center; flex-direction: row; flex-wrap: wrap;"
         
-        // let flexElements = document.getElementsByClassName('.flex__element');
-        // console.log(flexElements)
-        // for (let i=0; i<flexElements; i++) {
-        //     flexElements[i].style.cssText = "margin: 10px; padding: 10px; background-color: blue;  border: 3px solid black; border-radius: 10px;"
-        // }
-
     }
 }
