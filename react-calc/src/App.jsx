c5285e3589947ca6aefb8be79df86431812a9f3a
Monohansett <Frostyvi92@gmail.com>
2018-10-11 18:57:07 +0300

    implented calc basic operations

--- react-calc/src/App.jsx
+++ react-calc/src/App.jsx
@@ -1,11 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+// import propTypes from 'prop-types';
 
 class App extends Component {
+
+    state = {
+        operationType: 'addition',
+        leftOperand: '',
+        rightOperand: '',
+        result: ''
+    }
+
+    handleLeftOperand = (evt) => {
+        this.setState({
+            leftOperand: evt.target.value
+        })
+    }
+
+    handleRightOperand = (evt) => {
+        this.setState({
+            rightOperand: evt.target.value
+        })
+    }
+
+    handleChangeOperations = (evt) => {
+        this.setState({
+            operationType: evt.target.value
+        })
+    }
+
+    ToCosoleResult = () => {
+        if (this.state.operationType === 'addition') {
+            this.setState({
+                result: parseInt(this.state.leftOperand) + parseInt(this.state.rightOperand)
+            })
+        } else if (this.state.operationType === 'subtraction') {
+            this.setState({
+                result: parseInt(this.state.leftOperand) - parseInt(this.state.rightOperand)
+            })
+        } else if (this.state.operationType === 'multiply') {
+            this.setState({
+                result: parseInt(this.state.leftOperand) * parseInt(this.state.rightOperand)
+            })
+        } else {
+            this.setState({
+                result: parseInt(this.state.leftOperand) / parseInt(this.state.rightOperand)
+            })
+        }
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div className="container">
+                <div className="inputs__values">
+                    <input type="text" onChange={this.handleLeftOperand} required placeholder="left operand"/>
+                    <select placeholder="operation" onChange={this.handleChangeOperations}>
+                        <option value="addition">+</option>
+                        <option value="subtraction">-</option>
+                        <option value="multiply">*</option>
+                        <option value="division">/</option>
+                    </select>
+                    <input type="text" onChange={this.handleRightOperand} placeholder="right operand"/>
+                    <span><button onClick={this.ToCosoleResult}>=</button></span>
+                    <input type="text" value={this.state.result} placeholder="result"/>
+                </div>
+            </div>
         )
     }
 }
+
+// App.propTypes = {
+//     leftOperand: propTypes.number.isRequired,
+//     rightOperand: propTypes.number.isRequired
+// }
 
 export default App;
